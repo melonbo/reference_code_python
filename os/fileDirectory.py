ac72d9660f74c0b88a47a7602fddcb4c12acc198
melonbo <guliqun1983@163.com>
2023-06-28 18:55:18 +0800

    Update fileDirectory.py
    文件、目录操作

--- os/fileDirectory.py
+++ os/fileDirectory.py
@@ -9,4 +9,13 @@ def getNameList(dir, outputFile):
             # print(os.path.splitext(file)[0])
             f.write(os.path.splitext(file)[0] + '\n')
 
-
+# 遍历目录下文件
+def listfiles(rootDir):
+  list_dirs = os.walk(rootDir)
+  for root, dirs, files in list_dirs:
+    for d in dirs:
+      print(os.path.join(root,d))
+    for f in files:
+      fileid = f.split('.')[0]
+      filepath = os.path.join(root,f)
+      print(filepath)
